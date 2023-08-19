400fe69255cba32edbf4b1b145bab5f43ba37752
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-08-19 19:39:50 -0400

    Added Footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -2,8 +2,25 @@ import './footer.css';
 
 function Footer() {
     return (
-        <div className="footer__container">
-            
+        <div className="footer">
+            <div className="start__project__container">
+                <div className="start__project__box">
+                    <div className="start__project__content">
+                        <div className="start__project">
+                            <h2>Start a Project</h2>
+                        </div>
+                        <div className="interested">
+                            <h3>Interested in working together? We should queue up a time to chat. I&apos;m open for exciting ventures.</h3>
+                        </div>
+                        <div className="let__do__this">
+                            <a href='mailto:prabhjot.sains@gmail.com'>Let&apos;s do this</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__container">
+                <h1>Hello</h1>
+            </div>
         </div>
     );
 }
