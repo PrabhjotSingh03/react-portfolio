5c8f57421ec0533fdc04555b800d0969c9b61d02
PrabhjotSingh03 <prabhjot.sains@gmail.com>
2023-10-11 22:31:09 -0400

    Removed Api Fetch from experience now only static

--- components/experience/experience.jsx
+++ components/experience/experience.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 import './experience.css';
 
 function Experience() {
-    const [experienceData, setexperienceData] = useState([]);
+    // const [experienceData, setexperienceData] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://cyberprabhjot.co.in/api/data')
-            .then(response => {
-                const reversedData = response.data.reverse();
-                setexperienceData(reversedData);
-            })
-            .catch(error => {
-                console.error('Error fetching skills:', error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     axios.get('https://cyberprabhjot.co.in/api/data')
+    //         .then(response => {
+    //             const reversedData = response.data.reverse();
+    //             setexperienceData(reversedData);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching skills:', error);
+    //         });
+    // }, []);
 
-    const experienceInCategory = experienceData.filter(item => item.category === 'experience');
+    // const experienceInCategory = experienceData.filter(item => item.category === 'experience');
 
     return (
         <div className='experience__card'>
@@ -27,7 +27,7 @@ function Experience() {
                     <h3>An aspiring Full stack web developer with a passion for crafting seamless digital experiences. Beyond coding, I thrive on community involvement, actively engaging with open-source projects and event organization. My goal is to craft exceptional web applications that merge functionality with creativity, pushing the boundaries of what technology can achieve.</h3>
                 </div>
             </div>
-            <div className="experience__container__main">
+            {/* <div className="experience__container__main">
                 <div className="experience__card__main">
                     {experienceInCategory.map(experience => (
                         <div className="experience__card__container" key={experience._id.$oid}>
@@ -58,7 +58,63 @@ function Experience() {
                         </div>
                     ))}
                 </div>
-            </div>
+            </div> */}
+            <div className="experience__container__main">
+                <div className="experience__card__main">
+                    <div className="experience__card__container">
+                        <div className='experience__image__container'>
+                            <img className="experience__image" src='https://extechdigital.in/wp-content/uploads/2023/03/cropped-logonew-removebg-preview-2-165x50.png' alt='Extech Digital Logo' />
+                        </div>
+                        <div className="experience__details">
+                            <div className='company__title__container'>
+                                <div className='position__title__container'>
+                                    <h3 className="experience__position__title">Web Developer</h3>
+                                </div>
+                                <div className='position__work__time'>
+                                    <h4 className="experience__work__time">January 2018 - July 2021</h4>
+                                </div>
+                            </div>
+                            <div className='company__title__container'>
+                                <div className='position__title__container'>
+                                    <h4 className="experience__company__title">
+                                        <a href='https://extechdigital.in/'>Extech Digital</a>
+                                    </h4>
+                                </div>
+                                <div className='position__work__time'>
+                                    <h4>Mohali, Punjab, India</h4>
+                                </div>
+                            </div>
+                            <p className="experience__position__description">During my tenure at Extech Digital, I had the privilege of contributing to the development team as a front-end developer. My primary responsibilities included crafting website layouts and user interfaces through the proficient use of HTML and CSS. I seamlessly integrated data from diverse back-end services and databases, ensuring seamless functionality and a seamless user experience. My commitment to best development practices resulted in well-designed and highly efficient code. Moreover, I excelled in gathering and refining specifications and requirements, aligning them with technical needs to drive project success. Additionally, I played a vital role in creating and maintaining comprehensive software documentation, facilitating collaboration and knowledge sharing within the team.</p>
+                        </div>
+                    </div>
+                    <div className="experience__card__container">
+                        <div className='experience__image__container'>
+                            <img className="experience__image" src='/images/experience/swat.png' alt='Student&apos;s Welfare through Advance Technology(SWAT)Logo' />
+                        </div>
+                        <div className="experience__details">
+                            <div className='company__title__container'>
+                                <div className='position__title__container'>
+                                    <h3 className="experience__position__title">Founder and Chair-person</h3>
+                                </div>
+                                <div className='position__work__time'>
+                                    <h4 className="experience__work__time">May 2015 - May 2017</h4>
+                                </div>
+                            </div>
+                            <div className='company__title__container'>
+                                <div className='position__title__container'>
+                                    <h4 className="experience__company__title">
+                                        <a href='#'>Student&apos;s Welfare through Advance Technology(SWAT)</a>
+                                    </h4>
+                                </div>
+                                <div className='position__work__time'>
+                                    <h4>Baddi University, HP, India</h4>
+                                </div>
+                            </div>
+                            <p className="experience__position__description"> A vibrant society with a key focus on the Internet of Things (IoT). Its mission was to cultivate a collaborative space where tech enthusiasts, professionals, and students come together to explore, create, and propel the possibilities of IoT and related technologies. Through workshops, projects, and interdisciplinary collaboration, members enhance their skills, develop innovative solutions, and address real-world challenges. With a strong commitment to ethical considerations, industry networking, and community outreach, SWAT was not just a society—it was a dynamic hub for shaping the future of technology.</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
         </div>
     );
 }
