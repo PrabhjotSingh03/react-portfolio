3fde6c08145e85b7d8aef74b283a0a138ca0a88e
PrabhjotSingh03 <prabhjot.sains@gmail.com>
2023-10-24 22:53:23 -0400

    Edited

--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -57,7 +57,7 @@ function Projects() {
                     <img className="project__image" src='/images/projects/urbanclothing.jpg' alt='Urban Clothing' />
                     <div className="project__details">
                         <h3 className="project__title">Clothing Store UI</h3>
-                        <p className="project__description">An E-Commerce clothing website for shopaholics. Still Under Development</p>
+                        <p className="project__description">An E-Commerce clothing website for shopaholics</p>
                         <p className="project__languages">NodeJS, Pug, ExpressJS, MongoDb and APIs</p>
                         <a className="project__link" href='http://ec2-44-209-219-5.compute-1.amazonaws.com:5991/' target="_blank" rel="noopener noreferrer">
                             Visit Website
