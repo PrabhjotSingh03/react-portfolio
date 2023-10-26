ebbc22cb6bb2ff9987eece101a1f35f027ae4953
PrabhjotSingh03 <prabhjot.sains@gmail.com>
2023-10-26 00:54:03 -0400

    Improved UI in about and skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Nav from '../components/nav/nav';
 import Header from '../components/header/header';
 import About from '../components/about/about';
-import Skills from '../components/skills/skills';
 import Projects from '../components/projects/projects';
 import Experience from '../components/experience/experience';
 import Education from '../components/Education/education';
@@ -15,7 +14,6 @@ function App() {
       <Nav />
       <Header />
       <About />
-      <Skills />
       <Projects /> 
       <Experience />
       <Education />
